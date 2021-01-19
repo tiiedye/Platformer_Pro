6d0a634aa7091de1645fa57b85fccefe19ea7b61
tiiedye <tiiedye@gmail.com>
2021-01-19 11:00:52 -0800

    Setting Player's Velocity
    Adding speed and Velocity to the player's movement.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private CharacterController _controller;
+    [SerializeField]
+    private float _playerSpeed = 5.0f;
+
     void Start()
     {
         _controller = GetComponent<CharacterController>();
@@ -13,9 +16,12 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horzInput = Input.GetAxis("Horizontal");
-        Vector3 direction = new Vector3(horzInput, 0, 0);
+        // Getting Horizontal Input.
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Vector3 direction = new Vector3(horizontalInput, 0, 0);
+        // Setting Player's Velocity.
+        Vector3 velocity = direction * _playerSpeed;
 
-        _controller.Move(direction * Time.deltaTime);
+        _controller.Move(velocity * Time.deltaTime);
     }
 }
