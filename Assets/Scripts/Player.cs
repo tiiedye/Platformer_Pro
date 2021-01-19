38de5c0696cca9df9cf3a82d2920b043be8a4cab
tiiedye <tiiedye@gmail.com>
2021-01-19 10:52:30 -0800

    Setting up Character Controller
    Setting up Character controller and binding horizontal movement to A and D or right and left arrow keys.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController _controller;
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horzInput = Input.GetAxis("Horizontal");
+        Vector3 direction = new Vector3(horzInput, 0, 0);
+
+        _controller.Move(direction * Time.deltaTime);
+    }
+}
