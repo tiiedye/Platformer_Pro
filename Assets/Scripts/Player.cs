f20588ff7932ad8fa2602100e140f8800b53fa09
tiiedye <tiiedye@gmail.com>
2021-01-19 11:11:45 -0800

    Creating Gravity
    Creating Gravity and applying it to the player. Also adding a check if the player is grounded or not.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
     private CharacterController _controller;
     [SerializeField]
     private float _playerSpeed = 5.0f;
+    private float _gravity = 1.0f;
 
     void Start()
     {
@@ -16,11 +17,22 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        bool grounded = _controller.isGrounded;
         // Getting Horizontal Input.
         float horizontalInput = Input.GetAxis("Horizontal");
+
         Vector3 direction = new Vector3(horizontalInput, 0, 0);
         // Setting Player's Velocity.
         Vector3 velocity = direction * _playerSpeed;
+
+        // checks if the player is grounded.
+        if (grounded == true) {
+            // do nothing, maybe jump later
+            return;
+        } else {
+            // applies gravity if the player is not grounded.
+            velocity.y -= _gravity;
+        }
 
         _controller.Move(velocity * Time.deltaTime);
     }
